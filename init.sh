e419bbcabc402b305ccae25ef1969712ef028b7f
lilerong <lilerong@kuaishou.com>
2026-02-11 15:01:11 +0800

    完善环境脚本

--- init.sh
+++ init.sh
@@ -14,10 +14,10 @@ pip install imagesize
 
 #  配置oss，以便拉取训练数据和测试数据，我这里使用的是linux-amd64系列；
 #  这里进入到oss到项目根目录下，进行oss环境配置；
-cd ossutil-2.1.2-linux-amd64
-chmod 755 ossutil
-sudo mv ossutil /usr/local/bin/ && sudo ln -s /usr/local/bin/ossutil /usr/bin/ossutil
-ossutil   #这里出现oss的版本信息则说明配置成功
-
-# 这里进行oss 配置文件设置
-ossutil config  # 这一步可以参考天池下载数据那块oss具体配置  
+#cd ossutil-2.1.2-linux-amd64
+#chmod 755 ossutil
+#sudo mv ossutil /usr/local/bin/ && sudo ln -s /usr/local/bin/ossutil /usr/bin/ossutil
+#ossutil   #这里出现oss的版本信息则说明配置成功
+#
+## 这里进行oss 配置文件设置
+#ossutil config  # 这一步可以参考天池下载数据那块oss具体配置
